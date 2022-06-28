0f19cae70c284a79a1bd97cae5b42ddc48735b38
Stanislav Surskiy <ssurskiy@gmail.com>
2022-06-28 20:18:25 +0300

    fix(install.sh): set correct link for the commit-message script

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-repo_url="https://gitlab.com/admitad-projects/tools/git-hooks.git"
-release_tag=master
-script_file="https://raw.githubusercontent.com/craicoverflow/sailr/$release_tag/sailr.sh"
-
-
 if [ -n "$1" ]; then
   echo "Unsupported argument '$1'"
 fi
 
 destination="${PWD}/.git/hooks"
 
-curl https://gitlab.com/admitad-projects/tools/git-hooks/-/raw/master/hooks/commit-msg -o "${destination}/commit-msg"
+curl -Ls https://raw.githubusercontent.com/anonymous-development-projects/git-hooks/master/hooks/commit-msg -o "${destination}/commit-msg"
 chmod u+x "${destination}/commit-msg"
 
 echo "Installed git hook as commit-msg hook in ${destination}."
-echo "For usage see https://github.com/craicoverflow/sailr#usage"
+echo "For usage see https://guidelines.adprojects.ru/development/git-commit/"
